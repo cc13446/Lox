13c9b5b431be9126b3672de145a598244937d2d5
cc13446 <1344613920@qq.com>
2023-10-08 21:08:04 +0800

    分词 test

--- src/test/groovy/com/cc/loc/token/ScannerTest.groovy
+++ src/test/groovy/com/cc/loc/token/ScannerTest.groovy
@@ -1,28 +1,31 @@
 package com.cc.loc.token
 
 import com.cc.lox.token.Scanner
-import com.cc.lox.token.Token
-import com.cc.lox.token.type.TokenType
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.util.stream.Collectors
-
-
 /**
  * @author cc
  * @date 2023/10/8
  */
 class ScannerTest extends Specification {
 
-    def "test token" () {
+    @Unroll
+    def "test token #source #result"() {
         given:
-        def source = "var a = 1.11"
         def target = new Scanner(source)
 
         when:
         target.scanTokens()
-        def res = target.getResult().stream().map {a -> a.getType()}.collect(Collectors.toList())
+        def res = target.getResult().stream().map { a -> a.getLexeme() }.collect(Collectors.toList())
         then:
-        res == [TokenType.VAR, TokenType.IDENTIFIER, TokenType.EQUAL, TokenType.NUMBER, TokenType.EOF]
+        res == result
+
+        where:
+        source                  | result
+        """var a = 1.11 """     | ["var", "a", "=", "1.11", ""]
+        """var b = "1.11" """   | ["var", "b", "=", "\"1.11\"", ""]
+        """fun foo(var a) {}""" | ["fun", "foo", "(", "var", "a", ")", "{", "}", ""]
     }
 }
